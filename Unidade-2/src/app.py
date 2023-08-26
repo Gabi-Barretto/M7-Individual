95797464fc17dffb1a89197c9827758bd1e27cce
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2023-08-26 14:35:46 -0300

    Finalizacoes

--- Unidade-2/src/app.py
+++ Unidade-2/src/app.py
@@ -3,20 +3,39 @@ import psycopg2
 import hashlib
 import os
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
+
+
 def check_login(email, password):
-    conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db", port="5432")
+    try:
+        conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db", port="5432")
+    except Exception as e:
+        logging.debug(f"Erro ao conectar ao banco de dados: {e}")
+        return None
+
     cursor = conn.cursor()
     cursor.execute("SELECT id_pessoa, senha FROM pessoas WHERE email = %s", (email,))
     user = cursor.fetchone()
+
+    logging.debug(f"Usuário encontrado: {user}")
+
     conn.close()
 
     if user:
         user_id, stored_password = user
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        if hashed_password == stored_password:
-            return user_id
+        logging.debug(f"Senha fornecida (hash): {hashed_password}")
+        logging.debug(f"Senha armazenada (hash): {stored_password}")
+
+        return user_id
+        
     return None
 
 @app.route('/', methods=['GET'])
@@ -30,7 +49,7 @@ def login():
     
     user_id = check_login(email, password)
 
-    print(user_id)
+    logging.debug(f"ID do usuário: {user_id}")
 
     if user_id:
         token = hashlib.sha256(os.urandom(24)).hexdigest()
@@ -48,9 +67,11 @@ def todo_list():
 
     user_id = session['user_id']
 
+    logging.debug(f"ID do usuário: {user_id}")
+
     conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db", port="5432")
     cursor = conn.cursor()
-    cursor.execute("SELECT task FROM tasks WHERE user_id = %s", (user_id))
+    cursor.execute("SELECT task FROM tasks WHERE id_pessoa = %s", (user_id,))
     tasks = [row[0] for row in cursor.fetchall()]
     conn.close()
 
