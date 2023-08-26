d1677e8e6ac1cde86fd7341c8e39874cf802e987
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2023-08-26 12:50:16 -0300

    update ponderada 2

--- Unidade-2/src/app.py
+++ Unidade-2/src/app.py
@@ -1,30 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, render_template_string, redirect, session, render_template
 import psycopg2
+import hashlib
+import os
 
 app = Flask(__name__)
 
-def get_tasks():
-    conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db")
+def check_login(email, password):
+    conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db", port="5432")
     cursor = conn.cursor()
-    cursor.execute("SELECT task FROM tasks")
+    cursor.execute("SELECT id_pessoa, senha FROM pessoas WHERE email = %s", (email,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        user_id, stored_password = user
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        if hashed_password == stored_password:
+            return user_id
+    return None
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('./login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    
+    user_id = check_login(email, password)
+
+    print(user_id)
+
+    if user_id:
+        token = hashlib.sha256(os.urandom(24)).hexdigest()
+        session['user_id'] = user_id
+        session['token'] = token
+        return redirect('/todo')
+    else:
+        return "Login falhou!"
+
+
+@app.route('/todo')
+def todo_list():
+    if 'user_id' not in session or 'token' not in session:
+        return redirect('/login')
+
+    user_id = session['user_id']
+
+    conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db", port="5432")
+    cursor = conn.cursor()
+    cursor.execute("SELECT task FROM tasks WHERE user_id = %s", (user_id))
     tasks = [row[0] for row in cursor.fetchall()]
     conn.close()
-    return tasks
 
-@app.route('/')
-def index():
-    tasks = get_tasks()
-    return render_template('./html/index.html', tasks=tasks)
+    return render_template_string("<ul>{% for task in tasks %}<li>{{ task }}</li>{% endfor %}</ul>", tasks=tasks)
 
-@app.route('/add', methods=['POST'])
-def add_task():
-    task = request.form['task']
-    conn = psycopg2.connect(dbname="todolist", user="user", password="password", host="db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (task) VALUES (%s)", (task,))
-    conn.commit()
-    conn.close()
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
